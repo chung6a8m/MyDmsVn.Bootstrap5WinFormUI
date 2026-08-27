490b580bc12b477093cc27f9a363f282ec2c45bc
chungpd <chung6a8m@yahoo.com>
2026-08-27 14:40:33 +0700

    feat: add BootstrapButton size contract

---
+++ src/MyDmsVn.Bootstrap5WinFormUI/Controls/BootstrapButtonSize.cs
@@ -0,0 +1,16 @@
+namespace MyDmsVn.Bootstrap5WinFormUI.Controls;
+
+/// <summary>
+/// Defines the Bootstrap-inspired size presets available to <see cref="BootstrapButton"/>.
+/// </summary>
+public enum BootstrapButtonSize
+{
+    /// <summary>Compact button sizing.</summary>
+    Small,
+
+    /// <summary>Default desktop button sizing.</summary>
+    Default,
+
+    /// <summary>Large button sizing.</summary>
+    Large
+}
