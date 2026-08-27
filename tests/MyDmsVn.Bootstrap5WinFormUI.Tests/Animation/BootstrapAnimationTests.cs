197fd4bd2f3f8da014912592bbf90e6b1788d557
chungpd <chung6a8m@yahoo.com>
2026-08-27 13:14:17 +0700

    test: disambiguate animation assertions on net48

--- tests/MyDmsVn.Bootstrap5WinFormUI.Tests/Animation/BootstrapAnimationTests.cs
+++ tests/MyDmsVn.Bootstrap5WinFormUI.Tests/Animation/BootstrapAnimationTests.cs
@@ -13,8 +13,11 @@ public sealed class BootstrapAnimationTests
     [Test]
     public void ConstructorRejectsNonPositiveDuration()
     {
-        Assert.That(() => new BootstrapAnimation(TimeSpan.Zero), Throws.TypeOf<ArgumentOutOfRangeException>());
-        Assert.That(() => new BootstrapAnimation(TimeSpan.FromMilliseconds(-1)), Throws.TypeOf<ArgumentOutOfRangeException>());
+        Action zeroDuration = () => { using var animation = new BootstrapAnimation(TimeSpan.Zero); };
+        Action negativeDuration = () => { using var animation = new BootstrapAnimation(TimeSpan.FromMilliseconds(-1)); };
+
+        Assert.That(zeroDuration, Throws.TypeOf<ArgumentOutOfRangeException>());
+        Assert.That(negativeDuration, Throws.TypeOf<ArgumentOutOfRangeException>());
     }
 
     [Test]
@@ -220,8 +223,9 @@ public sealed class BootstrapAnimationTests
 
         animation.Start();
         clock.Advance(TimeSpan.FromMilliseconds(100));
+        Action fireFrame = scheduler.FireFrame;
 
-        Assert.DoesNotThrow(() => scheduler.FireFrame());
+        Assert.DoesNotThrow(fireFrame);
         Assert.That(animation.IsRunning, Is.False);
         animation.Dispose();
     }
@@ -254,11 +258,15 @@ public sealed class BootstrapAnimationTests
     {
         var animation = CreateAnimation(out _, out _);
         animation.Dispose();
-        Assert.DoesNotThrow(animation.Dispose);
+        Action dispose = animation.Dispose;
+        Action start = animation.Start;
+        Action stop = animation.Stop;
+        Action restart = animation.Restart;
 
-        Assert.That(animation.Start, Throws.TypeOf<ObjectDisposedException>());
-        Assert.That(animation.Stop, Throws.TypeOf<ObjectDisposedException>());
-        Assert.That(animation.Restart, Throws.TypeOf<ObjectDisposedException>());
+        Assert.DoesNotThrow(dispose);
+        Assert.That(start, Throws.TypeOf<ObjectDisposedException>());
+        Assert.That(stop, Throws.TypeOf<ObjectDisposedException>());
+        Assert.That(restart, Throws.TypeOf<ObjectDisposedException>());
     }
 
     private static BootstrapAnimation CreateAnimation(
