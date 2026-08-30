162e5ef6b64c9ef54813b568074b983b47961b47
chungpd <chung6a8m@yahoo.com>
2026-08-31 06:45:45 +0700

    fix: preserve BootstrapSelect API baseline

--- src/MyDmsVn.Bootstrap5WinFormUI/Controls/BootstrapSelect.Focus.cs
+++ src/MyDmsVn.Bootstrap5WinFormUI/Controls/BootstrapSelect.Focus.cs
@@ -4,17 +4,8 @@ namespace MyDmsVn.Bootstrap5WinFormUI.Controls;
 
 public partial class BootstrapSelect
 {
-    /// <inheritdoc />
-    protected override void OnGotFocus(EventArgs e)
+    private void OnFocusStateChanged(object? sender, EventArgs e)
     {
-        base.OnGotFocus(e);
-        Invalidate();
-    }
-
-    /// <inheritdoc />
-    protected override void OnLostFocus(EventArgs e)
-    {
-        base.OnLostFocus(e);
         Invalidate();
     }
 }
