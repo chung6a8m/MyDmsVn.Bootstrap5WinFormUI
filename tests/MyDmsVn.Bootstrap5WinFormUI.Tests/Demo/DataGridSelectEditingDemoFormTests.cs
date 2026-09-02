1c8b323120351417c931234cec3cda51f7342211
chungpd <chung6a8m@yahoo.com>
2026-09-02 09:59:51 +0700

    test: define DataGrid Select editing demo contract

---
+++ tests/MyDmsVn.Bootstrap5WinFormUI.Tests/Demo/DataGridSelectEditingDemoFormTests.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using MyDmsVn.Bootstrap5WinFormUI.Controls;
+using MyDmsVn.Bootstrap5WinFormUI.Demo;
+using NUnit.Framework;
+
+namespace MyDmsVn.Bootstrap5WinFormUI.Tests.Demo;
+
+[TestFixture]
+[Apartment(ApartmentState.STA)]
+public sealed class DataGridSelectEditingDemoFormTests
+{
+    [Test]
+    public void DemoUsesFiveNativeTextColumnsAndSampleRows()
+    {
+        using var form = CreateDemoForm();
+        form.CreateControl();
+        form.PerformLayout();
+
+        var grid = FindControls<BootstrapDataGridView>(form).Single();
+        var headers = grid.Columns.Cast<DataGridViewColumn>().Select(column => column.HeaderText).ToArray();
+
+        Assert.Multiple((Action)(() =>
+        {
+            Assert.That(grid.AutoGenerateColumns, Is.False);
+            Assert.That(grid.Columns.Cast<DataGridViewColumn>().All(column => column is DataGridViewTextBoxColumn), Is.True,
+                "The demo must not introduce a custom DataGridView column/cell type.");
+            Assert.That(headers, Is.EqualTo(new[] { "Tên hàng", "Đơn vị tính", "Số lượng", "Đơn giá", "Thành tiền" }));
+            Assert.That(grid.Rows.Count, Is.GreaterThan(0));
+        }));
+    }
+
+    [Test]
+    public void ProductCellEditingShowsBootstrapSelectAndCommitsProductMetadata()
+    {
+        using var form = CreateDemoForm();
+        form.CreateControl();
+        form.PerformLayout();
+
+        var grid = FindControls<BootstrapDataGridView>(form).Single();
+        grid.CreateControl();
+        grid.CurrentCell = grid.Rows[0].Cells["ProductNameColumn"];
+
+        Assert.That(grid.BeginEdit(true), Is.True);
+        Application.DoEvents();
+
+        var select = FindControls<BootstrapSelect>(grid).SingleOrDefault(control => control.Visible);
+        Assert.That(select, Is.Not.Null, "EditingControlShowing should replace the visible product editor with BootstrapSelect.");
+
+        Assert.That(select!.SelectValue(2), Is.True);
+        Application.DoEvents();
+
+        var row = grid.Rows[0];
+        Assert.Multiple((Action)(() =>
+        {
+            Assert.That(row.Cells["ProductNameColumn"].Value, Is.EqualTo("Trà ô long cao sơn"));
+            Assert.That(row.Cells["UnitColumn"].Value, Is.EqualTo("Hộp 20 túi"));
+            Assert.That(Convert.ToDecimal(row.Cells["UnitPriceColumn"].Value), Is.EqualTo(128000m));
+            Assert.That(Convert.ToDecimal(row.Cells["QuantityColumn"].Value), Is.EqualTo(2m));
+            Assert.That(Convert.ToDecimal(row.Cells["LineTotalColumn"].Value), Is.EqualTo(256000m));
+        }));
+    }
+
+    [Test]
+    public void EditingQuantityRecalculatesLineTotal()
+    {
+        using var form = CreateDemoForm();
+        form.CreateControl();
+        form.PerformLayout();
+
+        var grid = FindControls<BootstrapDataGridView>(form).Single();
+        var row = grid.Rows[0];
+
+        row.Cells["QuantityColumn"].Value = 3m;
+        grid.EndEdit();
+        Application.DoEvents();
+
+        Assert.That(Convert.ToDecimal(row.Cells["LineTotalColumn"].Value), Is.EqualTo(555000m));
+    }
+
+    private static Form CreateDemoForm()
+    {
+        var demoType = typeof(MainForm).Assembly.GetType("MyDmsVn.Bootstrap5WinFormUI.Demo.DataGridSelectEditingDemoForm");
+        Assert.That(demoType, Is.Not.Null, "The integrated demo should include DataGridSelectEditingDemoForm.");
+        return (Form)Activator.CreateInstance(demoType!)!;
+    }
+
+    private static IEnumerable<T> FindControls<T>(Control root)
+        where T : Control
+    {
+        foreach (Control child in root.Controls)
+        {
+            if (child is T match)
+            {
+                yield return match;
+            }
+
+            foreach (var nested in FindControls<T>(child))
+            {
+                yield return nested;
+            }
+        }
+    }
+}
