fe53f070d81672d056b0af136fcca3824b2f78b6
chungpd <chung6a8m@yahoo.com>
2026-09-03 18:41:27 +0700

    test: cover fixed-width checkable wrapping

--- tests/MyDmsVn.Bootstrap5WinFormUI.Tests/Controls/BootstrapCheckableMultilineTests.cs
+++ tests/MyDmsVn.Bootstrap5WinFormUI.Tests/Controls/BootstrapCheckableMultilineTests.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Threading;
 using System.Windows.Forms;
 using MyDmsVn.Bootstrap5WinFormUI.Controls;
+using MyDmsVn.Bootstrap5WinFormUI.Theme;
 using NUnit.Framework;
 
 namespace MyDmsVn.Bootstrap5WinFormUI.Tests.Controls;
@@ -46,20 +47,47 @@ public sealed class BootstrapCheckableMultilineTests
             $"{controlType.Name} must preserve explicit line breaks in preferred-size measurement.");
     }
 
-    [Test]
-    public void NarrowFixedWidthTextMeasurementWrapsInsteadOfForcingSingleLine()
+    [TestCase(typeof(BootstrapCheckBox))]
+    [TestCase(typeof(BootstrapRadioButton))]
+    [TestCase(typeof(BootstrapSwitch))]
+    public void AutoSizeFalseNarrowBoundsKeepNativeWordWrapping(Type controlType)
     {
-        using var font = new Font("Segoe UI", 9f);
-        const string text = "This label should wrap inside a narrow host-assigned width";
-        var flags = BootstrapCheckableRenderLogic.GetTextFormatFlags(
-            ContentAlignment.TopLeft,
-            useMnemonic: true,
-            showKeyboardCues: true,
-            autoEllipsis: false,
-            rightToLeft: false);
-        var wrapped = TextRenderer.MeasureText(text, font, new Size(72, 200), flags);
-        var oneLineHeight = TextRenderer.MeasureText("Single line", font).Height;
+        using var control = (ButtonBase)Activator.CreateInstance(controlType)!;
+        control.AutoSize = false;
+        control.Size = new Size(120, 64);
+        control.Padding = Padding.Empty;
+        control.Text = "This label should wrap inside a narrow host-assigned width";
+        control.TextAlign = ContentAlignment.TopLeft;
 
-        Assert.That(wrapped.Height, Is.GreaterThan(oneLineHeight));
+        BootstrapCheckableKind kind;
+        ContentAlignment checkAlign;
+        if (control is BootstrapRadioButton radio)
+        {
+            kind = BootstrapCheckableKind.RadioButton;
+            radio.CheckAlign = ContentAlignment.MiddleLeft;
+            checkAlign = radio.CheckAlign;
+        }
+        else
+        {
+            var checkBox = (CheckBox)control;
+            kind = control is BootstrapSwitch ? BootstrapCheckableKind.Switch : BootstrapCheckableKind.CheckBox;
+            checkBox.CheckAlign = ContentAlignment.MiddleLeft;
+            checkAlign = checkBox.CheckAlign;
+        }
+
+        var dpi = control.DeviceDpi > 0 ? control.DeviceDpi : 96;
+        var metrics = BootstrapCheckableRenderLogic.GetMetrics(kind, BootstrapThemeManager.CurrentTheme.Metrics, dpi);
+        var layout = BootstrapCheckableRenderLogic.GetLayout(control.ClientRectangle, control.Padding, metrics, checkAlign, rightToLeft: false);
+        var flags = BootstrapCheckableRenderLogic.GetTextFormatFlags(control.TextAlign, control.UseMnemonic, showKeyboardCues: true, control.AutoEllipsis, rightToLeft: false);
+        var wrapped = TextRenderer.MeasureText(control.Text, control.Font, layout.TextBounds.Size, flags);
+        var oneLineHeight = TextRenderer.MeasureText(control.Text, control.Font, Size.Empty, flags & ~TextFormatFlags.WordBreak).Height;
+
+        Assert.Multiple((Action)(() =>
+        {
+            Assert.That(control.AutoSize, Is.False);
+            Assert.That(control.Size, Is.EqualTo(new Size(120, 64)));
+            Assert.That(layout.TextBounds.Width, Is.GreaterThan(0));
+            Assert.That(wrapped.Height, Is.GreaterThan(oneLineHeight), $"{controlType.Name} should wrap text inside a fixed narrow text field.");
+        }));
     }
 }
